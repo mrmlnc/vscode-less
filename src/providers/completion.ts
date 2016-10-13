bc52f9dd77deb2387930b305b336668eb0b844af
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-10-13 03:00:05 +0300

    Improve docPath in suggestions

--- src/providers/completion.ts
+++ src/providers/completion.ts
@@ -9,31 +9,37 @@ import {
 
 import { ISymbols } from '../types/common';
 
+function getDocumentPath(currentUri: string, symbolsUri: string): string {
+	const rootUri = path.dirname(currentUri);
+	const docPath = path.relative(rootUri, symbolsUri);
+
+	if (docPath === path.basename(currentUri)) {
+		return 'current';
+	}
+
+	return docPath;
+}
+
 export function doCompletion(currentUri: string, currentWord: string, symbolList: ISymbols[]): CompletionList {
 	const completions = CompletionList.create([], false);
-	const rootUri = path.dirname(currentUri);
 
 	if (currentWord === '@') {
 		symbolList.forEach((symbols) => {
 			symbols.variables.forEach((variable) => {
-				const docPath = path.relative(rootUri, symbols.document) || 'current';
-
 				completions.items.push({
 					label: variable.name,
 					kind: CompletionItemKind.Variable,
-					detail: 'value: ' + variable.value + ', ' + docPath
+					detail: variable.value + ', ' + getDocumentPath(currentUri, symbols.document)
 				});
 			});
 		});
 	} else if (currentWord === '.' || currentUri === '#') {
 		symbolList.forEach((symbols) => {
 			symbols.mixins.forEach((mixin) => {
-				const docPath = path.relative(rootUri, symbols.document) || 'current';
-
 				completions.items.push({
 					label: mixin.name,
 					kind: CompletionItemKind.Function,
-					detail: docPath,
+					detail: getDocumentPath(currentUri, symbols.document),
 					insertText: mixin.name + '({{_}});'
 				});
 			});
